815811124c868d4b882217e6a343b331bbd9d982
LiL <90929523+milkfirst@users.noreply.github.com>
2023-01-25 23:30:12 -0500

    extra body tag

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 console.log('JavaScipt is up and running! :D');
 
-const stickerGraphic = document.querySelector('#handMade');
+const stickerGraphic = document.querySelector('#handMade','#zestyLemonSlice');
 
 function logThisId()    {
     console.log('This element is selected:', this.id);
