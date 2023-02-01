013f6ec73fd0c85a9db169183b34363f9def01c9
LiL <90929523+milkfirst@users.noreply.github.com>
2023-02-01 14:31:41 -0500

    fixed html

--- js/main.js
+++ js/main.js
@@ -7,3 +7,13 @@ function logThisId()    {
 }
 
 stickerGraphic.addEventListener('click', logThisId);
+
+/* +console.log('JavaScipt is up and running! :D');
+
+const stickerGraphic = document.querySelector('#handMade');
+
+function logThisId()    {
+    console.log('This element is selected:', this.id);
+}
+
+stickerGraphic.addEventListener('click', logThisId); */
